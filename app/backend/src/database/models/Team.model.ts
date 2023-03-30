faf0d832c1c44abd0f5d3bd41ae8c0214eae9f88
Luana Portezan <luanaportezan@gmail.com>
2023-03-30 18:04:24 -0300

    feat: team.model.ts

---
+++ app/backend/src/database/models/Team.model.ts
@@ -0,0 +1,28 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  teamName: {
+    type: STRING,
+    field: 'team_name',
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
