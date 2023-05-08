5e559052b5c5f3331a2794d543fcfe6c0bf41fdf
Luana Portezan <luanaportezan@gmail.com>
2023-05-08 11:11:10 -0300

    feat: user.model.ts

---
+++ app/backend/src/database/models/user.model.ts
@@ -0,0 +1,45 @@
+import {
+  INTEGER,
+  Model,
+  STRING,
+} from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare readonly id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  tableName: 'users',
+  timestamps: false,
+});
+
+export default User;
