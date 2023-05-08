2e58af31a643aba66a93ea333f15c4490b0ab314
Luana Portezan <luanaportezan@gmail.com>
2023-05-08 12:20:33 -0300

    refactor: space in migrations users

--- app/backend/src/database/migrations/20230508121138-create-users.js
+++ app/backend/src/database/migrations/20230508121138-create-users.js
@@ -2,30 +2,30 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-   await queryInterface.createTable('users',{
-    id: {
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true,
-      type: Sequelize.INTEGER,
-    },
-    username: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    role: {
-      type: Sequelize.STRING,
-      allowNull: true
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    password: {
-      type: Sequelize.STRING,
-      allowNull: false
-    }
-   })
+    await queryInterface.createTable('users',{
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    })
   },
 
   down: async (queryInterface, _Sequelize) => {
