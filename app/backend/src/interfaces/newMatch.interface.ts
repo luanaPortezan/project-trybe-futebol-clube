8d6b64d0a6cfad1e2101bf9606e3e6a167ba2720
Luana Portezan <luanaportezan@gmail.com>
2023-05-09 08:30:50 -0300

    refactor: match.service and match.interface

--- app/backend/src/interfaces/newMatch.interface.ts
+++ app/backend/src/interfaces/newMatch.interface.ts
@@ -1,6 +1,16 @@
+import Match from '../database/models/match.model';
+
 export default interface INewMatch {
   homeTeamId: number
   awayTeamId: number
   homeTeamGoals: number
   awayTeamGoals: number
+}
+
+interface teamName {
+  teamName: string
+}
+export interface IMatches extends Match {
+  homeTeam: teamName;
+  awayTeam: teamName
 }
