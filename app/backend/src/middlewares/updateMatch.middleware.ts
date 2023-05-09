a91c6f379e1339e3921141fb9bbdb972296d4172
Luana Portezan <luanaportezan@gmail.com>
2023-05-09 08:11:53 -0300

    feat: upadate match and finalize match

--- app/backend/src/middlewares/updateMatch.middleware.ts
+++ app/backend/src/middlewares/updateMatch.middleware.ts
@@ -7,10 +7,6 @@ export default class CheckUpdateMatchFields {
     if (!homeTeamGoals || !awayTeamGoals) {
       return res.status(400).json({ message: 'All fields must be filled correctly' });
     }
-
-    if (typeof homeTeamGoals !== 'number' || typeof awayTeamGoals !== 'number') {
-      return res.status(401).json({ message: 'Invalid values' });
-    }
     next();
   };
 }
